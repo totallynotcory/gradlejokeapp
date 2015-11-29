dcd600c8aaacb954d501dd8ed6ce82b0e062a878
Cory Potwin <cpotwin@gmail.com>
2015-11-29 10:54:19 -0500

    Step 4: Add Functional Tests - Complete.  Test for Asynctask returning a string is successful.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -1,4 +1,4 @@
-/*
+
 package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
@@ -7,24 +7,18 @@ import android.test.InstrumentationTestCase;
 import android.util.Log;
 import android.util.Pair;
 
-import java.net.URL;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
-*/
 /**
  * Created by ctpotwin on 11/28/15.
- *//*
+ */
 
-public class AsyncTaskTest extends InstrumentationTestCase{
+
+public class AsyncTaskTest extends InstrumentationTestCase implements AsyncResponse {
 
     private final static String LOG_TAG = "AsyncTaskTest";
-    private static boolean called;
-    private final Context context = getInstrumentation().getContext();
 
     protected void setUp() throws Exception {
         super.setUp();
-        called = false;
     }
 
     protected void tearDown() throws Exception {
@@ -33,44 +27,17 @@ public class AsyncTaskTest extends InstrumentationTestCase{
 
     public final void testSuccessfulFetch() throws Throwable {
         // create  a signal to let us know when our task is done.
-        final CountDownLatch signal = new CountDownLatch(1);
-
-        // Execute the async task on the UI thread! THIS IS KEY!
-        runTestOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                new EndpointsAsyncTask() {
-
-                    @Override
-                    protected void onPostExecute (String result){
-
-                        assert(result.length() > 0);
-                    //    super.onPostExecute(result);
-
-                */
-/* This is the key, normally you would use some type of listener
-                 * to notify your activity that the async call was finished.
-                 *
-                 * In your test method you would subscribe to that and signal
-                 * from there instead.
-                 *//*
-
-                        signal.countDown();
-                    }
-                }.execute(new Pair<Context, Integer>(context, 1));
-
-            }
-        });
-
-	    */
-/* The testing thread will wait here until the UI thread releases it
-	     * above with the countDown() or 30 seconds passes and it times out.
-	     *//*
-
-            signal.await(10,TimeUnit.SECONDS);
-
-            assertTrue(called);
+        new EndpointsAsyncTask().execute(new Pair<AsyncResponse, Integer>(this, 1));
         }
 
+    /**
+     * when the Async task has finished, make sure out put is not null and returns a value
+     * @param output string returned from asynctask
+     */
+    @Override
+    public void processFinish(String output){
+        assert (output != null && output.length() > 0);
     }
-*/
+
+}
+
